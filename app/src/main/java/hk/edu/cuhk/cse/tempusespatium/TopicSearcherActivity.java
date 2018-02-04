9874518cfe7014ca1a08eeaee6fe31a5955dc619
Alexis Poon <mundanesurrealism@gmail.com>
2018-02-05 02:41:01 +0800

    Remember to use LinkedHashMap!!!

--- app/src/main/java/hk/edu/cuhk/cse/tempusespatium/TopicSearcherActivity.java
+++ app/src/main/java/hk/edu/cuhk/cse/tempusespatium/TopicSearcherActivity.java
@@ -6,9 +6,9 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
-import android.widget.ListAdapter;
 import android.widget.TextView;
 
 import com.beardedhen.androidbootstrap.BootstrapButton;
@@ -21,7 +21,11 @@ import org.xml.sax.SAXException;
 import java.io.IOException;
 import java.io.StringReader;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -41,36 +45,31 @@ import okhttp3.Response;
 public class TopicSearcherActivity extends AppCompatActivity {
 
     OkHttpClient mClient;
-    List<String> mUrls;
-    List<String> mTopics;
+    Map<String, String> mTopics;
+
+    Map<String, String> mArts;
+
+    AdapterView.OnItemClickListener mOnItemClickListener;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.topic_searcher);
 
-        mUrls = new ArrayList<>();
-        mTopics = new ArrayList<>();
+        mTopics = new HashMap<>();
 
         // TODO: !!!!!!!!!!!!!! https://en.wikipedia.org/wiki/Category:WikiProjects_by_topic !!!!!!!!!!!!!!!!!!!!!!!!
         // Talk, file talk...
-        BootstrapButton searchButton = (BootstrapButton) findViewById(R.id.topic_search_button);
-        searchButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
+//        BootstrapButton searchButton = (BootstrapButton) findViewById(R.id.topic_search_button);
+//        searchButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//            }
+//        });
 
         BootstrapButton submitButton = (BootstrapButton) findViewById(R.id.topic_submit_button);
-        submitButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent jump = new Intent(getBaseContext(), Round1Activity.class);
-                startActivity(jump);
-                finish();
-            }
-        });
+        submitButton.setEnabled(false);
 
         String base_url = "https://en.wikipedia.org/wiki/Wikipedia:Lists_of_popular_pages_by_WikiProject";
         mClient = new OkHttpClient();
@@ -98,10 +97,9 @@ public class TopicSearcherActivity extends AppCompatActivity {
                                 NodeList test = (NodeList) staticXPath.evaluate(doc, XPathConstants.NODESET);
                                 for (int i = 0; i < test.getLength(); i++) {
                                     //URL
-                                    mUrls.add("https://en.wikipedia.org" + test.item(i).getAttributes().item(0).getTextContent());
                                     Log.i("Test", test.item(i).getAttributes().item(0).getTextContent());
                                     //Topics
-                                    mTopics.add(test.item(i).getAttributes().item(1).getTextContent().replaceFirst("Wikipedia:(WikiProject )?", "").replaceFirst("/Popular pages", "").replaceFirst("( task force| work group)", "").replaceFirst("Taskforces/(BPH/)?", "").replaceFirst("/", " > "));
+                                    mTopics.put(test.item(i).getTextContent().replaceFirst("Wikipedia:(WikiProject )?", "").replaceFirst("/Popular pages", "").replaceFirst("( task force| work group)", "").replaceFirst("Taskforces/(BPH/)?", "").replaceFirst("/", " > "), "https://en.wikipedia.org" + test.item(i).getAttributes().item(0).getTextContent());
 //                                    Log.i("Test", test.item(i).getAttributes().item(1).getTextContent());
                                 }
 
@@ -114,9 +112,67 @@ public class TopicSearcherActivity extends AppCompatActivity {
                             } catch (XPathExpressionException e) {
                                 e.printStackTrace();
                             }
-                            ArrayAdapter<String> adapter = new ArrayAdapter<>(TopicSearcherActivity.this,android.R.layout.select_dialog_item, mTopics);
+                            final ArrayAdapter<String> adapter = new ArrayAdapter<>(TopicSearcherActivity.this, android.R.layout.simple_dropdown_item_1line, new ArrayList<String>(mTopics.keySet()));
                             AutoCompleteTextView autoCompleteTextView = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView);
                             autoCompleteTextView.setAdapter(adapter);
+                            autoCompleteTextView.showDropDown();
+                            mOnItemClickListener=new AdapterView.OnItemClickListener() {
+                                @Override
+                                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                                    mArts = new LinkedHashMap<>();
+                                    final Request request1 = new Request.Builder()
+                                            .url(mTopics.get(adapter.getItem(i)))
+                                            .build();
+                                    final Response[] response1 = {null};
+                                    final String[] body1 = {""};
+                                    Thread thread1 = new Thread(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            try {
+                                                response1[0] = mClient.newCall(request1).execute();
+                                                body1[0] = response1[0].body().string();
+
+                                                Document doc = DocumentBuilderFactory.newInstance()
+                                                        .newDocumentBuilder().parse(new InputSource(new StringReader(body1[0])));
+                                                XPathExpression staticXPath = XPathFactory.newInstance()
+                                                        .newXPath().compile("//*[@id=\"mw-content-text\"]/div/table/tr/td[2]/a");
+                                                NodeList test = (NodeList) staticXPath.evaluate(doc, XPathConstants.NODESET);
+                                                for (int i = 0; i < test.getLength(); i++) {
+                                                    mArts.put(test.item(i).getTextContent(), "https://en.wikipedia.org" + test.item(i).getAttributes().item(0).getTextContent());
+                                                }
+                                            } catch (SAXException e) {
+                                                e.printStackTrace();
+                                            } catch (IOException e) {
+                                                e.printStackTrace();
+                                            } catch (ParserConfigurationException e) {
+                                                e.printStackTrace();
+                                            } catch (XPathExpressionException e) {
+                                                e.printStackTrace();
+                                            }
+                                            runOnUiThread(new Runnable() {
+                                                @Override
+                                                public void run() {
+                                                    TextView textView = (TextView) findViewById(R.id.result);
+                                                    textView.setText(mArts.keySet().toString());
+
+                                                    BootstrapButton submitButton = (BootstrapButton) findViewById(R.id.topic_submit_button);
+                                                    submitButton.setEnabled(true);
+                                                    submitButton.setOnClickListener(new View.OnClickListener() {
+                                                        @Override
+                                                        public void onClick(View view) {
+                                                            Intent jump = new Intent(getBaseContext(), Round1Activity.class);
+                                                            startActivity(jump);
+                                                            finish();
+                                                        }
+                                                    });
+                                                }
+                                            });
+                                        }
+                                    });
+                                    thread1.start();
+                                }
+                            };
+                            autoCompleteTextView.setOnItemClickListener(mOnItemClickListener);
                         }
                     });
                 } catch (IOException e) {

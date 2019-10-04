379753045909a9252bdeef023ec54a17c0aeb741
Alex Poon <mundanesurrealism@gmail.com>
2019-10-04 15:50:22 +0800

    .

---
+++ ios/Tempus E Spatium/Tempus E SpatiumTests/Tempus_E_SpatiumTests.swift
@@ -0,0 +1,34 @@
+//
+//  Tempus_E_SpatiumTests.swift
+//  Tempus E SpatiumTests
+//
+//  Created by Victor Poon on 4/10/2019.
+//  Copyright © 2019 SoftFeta. All rights reserved.
+//
+
+import XCTest
+@testable import Tempus_E_Spatium
+
+class Tempus_E_SpatiumTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

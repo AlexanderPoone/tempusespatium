fd13e175659b882e1f29ef519966e0a9b1c9f635
Alex Poon <mundanesurrealism@gmail.com>
2019-10-09 13:31:59 +0800

    .

--- ios/Tempus E Spatium/Tempus E Spatium/TopicSearcherViewController.swift
+++ ios/Tempus E Spatium/Tempus E Spatium/TopicSearcherViewController.swift
@@ -8,6 +8,46 @@
 
 import UIKit
 import DropDown
+import Alamofire
+import Ono
+
+extension DataRequest {
+    static func xmlResponseSerializer() -> DataResponseSerializer<ONOXMLDocument> {
+        return DataResponseSerializer { request, response, data, error in
+            // Pass through any underlying URLSession error to the .network case.
+            guard error == nil else { return .failure(BackendError.network(error: error!)) }
+
+            // Use Alamofire's existing data serializer to extract the data, passing the error as nil, as it has
+            // already been handled.
+            let result = Request.serializeResponseData(response: response, data: data, error: nil)
+
+            guard case let .success(validData) = result else {
+                return .failure(BackendError.dataSerialization(error: result.error! as! AFError))
+            }
+
+            do {
+                let xml = try ONOXMLDocument(data: validData)
+                return .success(xml)
+            } catch {
+                return .failure(BackendError.xmlSerialization(error: error))
+            }
+        }
+    }
+
+    @discardableResult
+    func responseXMLDocument(
+        queue: DispatchQueue? = nil,
+        completionHandler: @escaping (DataResponse<ONOXMLDocument>) -> Void)
+        -> Self
+    {
+        return response(
+            queue: queue,
+            responseSerializer: DataRequest.xmlResponseSerializer(),
+            completionHandler: completionHandler
+        )
+    }
+}
+
 
 class TopicSearcherViewController: UIViewController {
     
@@ -25,6 +65,52 @@ class TopicSearcherViewController: UIViewController {
     @IBOutlet weak var mTopicDropDown: DropDown!
     
     @IBOutlet weak var mSelectAnyHeader: UILabel!
+    
+    func english() {
+        AF.request("\(mBaseUrl)BluetoothSyncAttendance", method: .get, parameters: [:], encoding: JSONEncoding.default)
+            .validate(statusCode: 200..<300)
+            .validate(contentType: ["application/json"])
+            .responseData { response in
+                switch response.result {
+                case .success(let value):
+                    
+                    guard let url = Bundle.main.url(forResource: "nutrition", withExtension: "html"),
+                        let data = try? Data(contentsOf: url) else
+                    {
+                        fatalError("Missing resource: nutrition.xml")
+                    }
+                    
+                    let document = try ONOXMLDocument(data: data)
+                    document.rootElement.tag
+                    
+                    for element in document.rootElement.children.first?.children ?? [] {
+                        let nutrient = element.tag
+                        let amount = element.numberValue!
+                        let unit = element.attributes["units"]!
+                        
+                        print("- \(amount)\(unit) \(nutrient)")
+                    }
+                    
+                    document.enumerateElements(withXPath: "//food/name") { (element, _, _) in
+                        print(element)
+                    }
+                case .failure(let error):
+                    break
+                }
+        }
+    }
+    
+    func francais() {
+        
+    }
+    
+    func espanol() {
+        
+    }
+    
+    func deutsch() {
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ ios/Tempus E Spatium/Tempus E Spatium/es_KeyboardViewController.swift
@@ -0,0 +1,30 @@
+//
+//  es_KeyboardViewController.swift
+//  Tempus E Spatium
+//
+//  Created by Victor Poon on 9/10/2019.
+//  Copyright © 2019 SoftFeta. All rights reserved.
+//
+
+import UIKit
+
+class es_KeyboardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
